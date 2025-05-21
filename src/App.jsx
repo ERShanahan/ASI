d23df31f83d4833a4089f3451ba1b907ddf2a656
ERShanahan <eshanaha@stevens.edu>
2025-05-21 16:32:49 -0400

    more vercel things

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Recorder from './pages/Recorder';
-import Learn from './pages/Learn'
 import './index.css';
 
 export default function App() {
